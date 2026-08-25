579fa6ba1b108d20202598bf852b43b251563222
Himanshu <himanshujangra9899@gmail.com>
2026-08-25 10:30:05 +0000

    variables

---
+++ basics/1_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "himanshu@gmail.com"
+var accountPassword = "123456"
+accountCity = "delhi"
+
+accountEmail = "jangra@gmail.com"
+accountPassword = "213465"
+accountCity = "dholakpur"
+
+console.log(accountId);
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
+//ho sake toh variables ka use ni krna 
